f5f5cebb87c66739eff07b42922b70634435fd6d
S-Flavius <76589861+S-Flavius@users.noreply.github.com>
2021-12-03 14:06:22 +0100

    Day 1, Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,23 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var lastNum = input[0].toInt()
+        var increases = 0
+        for (num in input) {
+            if (lastNum < num.toInt()) {
+                increases++
+            }
+            lastNum = num.toInt()
+        }
+        return increases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+       return 0
     }
 
-    // test if implementation meets criteria from the description, like:
+//     test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
